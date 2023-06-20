6fb308657dbd6e153ddafc1d4c23bbfb3a7e3c88
Alpha <alfasammani@gmail.com>
2023-06-19 19:00:48 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ *main -A program that prints all possible different combinations of two digits
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ones;
+	int tens;
+
+	for (tens = '0'; tens <= '9'; tens++) /*increment tens*/
+	{
+		for (ones = (tens + 1); ones <= '9'; ones++) /*one's ten+1*/
+		{
+			putchar(tens);
+			putchar(ones);
+
+			if (tens != '8' || ones != '9') /*print commas*/
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
